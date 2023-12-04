092164624bdc744add2182bef4c83d204fe1e3b4
salahelmekki <salah-omri@hotmail.com>
2023-12-04 22:02:46 +0100

    chat bot front

---
+++ scripts.js
@@ -0,0 +1,29 @@
+// scripts.js
+document.getElementById('send-btn').addEventListener('click', sendMessage);
+
+async function sendMessage() {
+  const userMessage = document.getElementById('user-input').value;
+  displayMessage(userMessage, 'user');
+  document.getElementById('user-input').value = '';  // Clear input field
+
+  // Send user message to the backend
+  const response = await fetch('http://localhost:8000/openai', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ messages: [{ role: 'user', content: userMessage }] }),
+  });
+
+  const responseData = await response.json();
+  const assistantReply = responseData.assistant_reply;
+  displayMessage(assistantReply, 'bot');
+}
+
+function displayMessage(message, sender) {
+  const chatDisplay = document.getElementById('chat-display');
+  const messageElement = document.createElement('div');
+  messageElement.className = `message ${sender}`;
+  messageElement.innerText = message;
+  chatDisplay.appendChild(messageElement);
+}
